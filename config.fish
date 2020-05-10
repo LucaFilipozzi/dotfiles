ff2635ddcf331ce21f0e831d2c687aadb482db07
Luca Filipozzi <luca.filipozzi@gmail.com>
2020-05-10 09:01:35 -0700

    remove exa aliases

--- config.fish
+++ config.fish
@@ -12,11 +12,3 @@ set -x fish_user_paths $HOME/bin $fish_user_paths
 set -x AWS_VAULT_BACKEND pass
 set -x AWS_VAULT_PASS_CMD /usr/local/bin/gopass
 
-alias ls 'exa --group --modified --time-style long-iso $argv'
-alias ll 'exa --group --modified --time-style long-iso --bytes --long $argv'
-alias la 'exa --group --modified --time-style long-iso --bytes --long --all $argv'
-alias ld 'exa --group --modified --time-style long-iso --bytes --long --all --list-dirs $argv'
-alias lg 'exa --group --modified --time-style long-iso --bytes --long --all --git --git-ignore $argv'
-alias lm 'exa --group --modified --time-style long-iso --bytes --long --all --git --git-ignore --sort modified $argv'
-alias lt 'exa --group --modified --time-style long-iso --bytes --long --all --tree --level $argv'
-
